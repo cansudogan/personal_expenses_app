045392784c389fc3f3feb8fc4b7a867a95978ba6
cansudogan <cansudogan95@gmail.com>
2021-01-24 02:25:35 +0300

    get input with controller

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,10 @@ class MyHomePage extends StatelessWidget {
       date: DateTime.now(),
     ),
   ];
+  //tring titleInput;
+  //String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -57,14 +61,23 @@ class MyHomePage extends StatelessWidget {
                 children: <Widget>[
                   TextField(
                     decoration: InputDecoration(labelText: 'Title'),
+                    controller: titleController,
+                    // onChanged: (val) => titleInput = val;
                   ),
                   TextField(
                     decoration: InputDecoration(labelText: 'Amount'),
+                    controller: amountController,
+                    // onChanged: (val) => amountInput = val;
                   ),
                   FlatButton(
-                    onPressed: () {},
                     child: Text('Add Transaction'),
                     textColor: Colors.purple[200],
+                    onPressed: () {
+                      print(titleController.text);
+                      print(amountController.text);
+                      //print(titleInput);
+                      //print(amountInput);
+                    },
                   ),
                 ],
               ),
