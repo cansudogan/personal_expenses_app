b429084038fadc181c46d861e79f50f6122cb40b
cansudogan <cansudogan95@gmail.com>
2021-01-25 01:20:19 +0300

    provide to add new transactions

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,7 +10,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: _userTransactions.map((tx) {
+      children: transactions.map((tx) {
         return Card(
           color: Colors.green[100],
           child: Row(
